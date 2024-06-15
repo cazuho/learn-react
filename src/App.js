e4e34dcd7d665e00ddd6fe4944bc30bab6a11e36
cazuho <cakumubom9210@gmail.com>
2024-06-15 10:00:59 +0900

    feat: App.jsにゲームボードを表示するコンポーネントを実装

--- src/App.js
+++ src/App.js
@@ -1,3 +1,31 @@
-export default function Square() {
-  return <button className="square">X</button>;
+// ボードの各マス目を表すコンポーネント
+function Square({ value }) {
+  return <button className="square">{value}</button>;
 }
+
+
+// このファイルのメイン関数 ゲーム盤を表示する
+export default function Board() {
+  // React コンポーネントから返される要素は、単一のJSX要素である必要があるので、 <></> で囲む
+  return (
+    <>
+      <div className="board-row">
+        {/* Squareコンポーネントを呼び出し、propsとしてvalueを渡す */}
+        <Square value="1"/>
+        <Square value="2"/>
+        <Square value="3"/>
+      </div>
+      <div className="board-row">
+        <Square value="4"/>
+        <Square value="5"/>
+        <Square value="6"/>
+      </div>
+      <div className="board-row">
+        <Square value="7"/>
+        <Square value="8"/>
+        <Square value="9"/>
+      </div>
+    </>
+  );
+}
+
